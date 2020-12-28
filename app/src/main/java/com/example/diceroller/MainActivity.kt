80d2a431cebfa4dc263cb8978ccd6bb5038e3ed2
Marilia Macedo <maritamisguemacedo@gmail.com>
2020-12-28 17:06:56 +0000

    First Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+
+/**
+ * This activity alows the user to roll a dice and view the result on the screen*/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+        //Do a dice roll when the app starts
+        rollDice()
+    }
+
+    /*Roll the dice and update the screen with the result*/
+    private fun rollDice() {
+        //Create new Dice object with 6 sides and roll it
+        val dice = Dice(6).roll()
+
+        val background:ConstraintLayout = findViewById(R.id.view_bkg)
+        background.setBackgroundColor(ContextCompat.getColor(this, getColorByPos((1..6).random())))
+
+        //Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        //Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = getDrawableResourceSide(dice)
+        //Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        //Update the content description for screen readers
+        diceImage.contentDescription = dice.toString()
+
+    }
+
+    private fun getDrawableResourceSide(side: Int) = when (side) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    private fun getColorByPos(colorPos: Int): Int = when (colorPos) {
+        1 -> R.color.purple_200
+        2 -> R.color.purple_700
+        3 -> R.color.teal_700
+        4 -> R.color.black
+        5 -> R.color.teal_200
+        else -> R.color.white
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
